7b55bbe3c2c0c5fe41ad9f3848971f2aac8b1b5a
Bicky7735369355 <153789263+Bicky7735369355@users.noreply.github.com>
2024-01-19 08:48:35 +0530

    Update WelcomeRestController.java

--- src/main/java/in/ashokit/rest/WelcomeRestController.java
+++ src/main/java/in/ashokit/rest/WelcomeRestController.java
@@ -18,7 +18,7 @@ public class WelcomeRestController {
 	@GetMapping
 	public String welcomeMsg() {
 		logger.info("***** welcomeMsg() execution start *****");
-		String msg = "Welcome to Ashok IT - Software Training Institute (HYD) ..!!";
+		String msg = "Welcome to Sahoo Family Bicky Lucky Tapu ..!!";
 		logger.info("***** welcomeMsg() execution end *****");
 		return msg;
 	}
